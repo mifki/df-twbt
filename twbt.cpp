a74aed3b1899a3b3d38f027d280b23adc943865c
Vitaliy Pronkin <pronvit@mifki.com>
2014-06-16 00:52:28 +1200

    fix for movie player screens making trade screen divide evenly on wide resolutions (OS X only) crash on plugin unload fixed

--- twbt.cpp
+++ twbt.cpp
@@ -14,6 +14,7 @@
 #include "Console.h"
 #include "Export.h"
 #include "PluginManager.h"
+#include "VTableInterpose.h"
 #include "modules/Maps.h"
 #include "modules/World.h"
 #include "modules/MapCache.h"
@@ -41,7 +42,9 @@
 #include "df/viewscreen_layer_export_play_mapst.h"
 #include "df/viewscreen_layer_world_gen_paramst.h"
 #include "df/viewscreen_overallstatusst.h"
+#include "df/viewscreen_tradegoodsst.h"
 #include "df/viewscreen_petst.h"
+#include "df/viewscreen_movieplayerst.h"
 #include "df/ui_sidebar_mode.h"
 
 #ifdef WIN32
@@ -195,11 +198,16 @@ bool is_text_tile(int x, int y, bool &is_map)
 
     is_map = false;
 
-    if (strict_virtual_cast<df::viewscreen_dwarfmodest>(ws))
-    {
-        if (!x || !y || x == w - 1 || y == h - 1)
-           return has_textfont;
+    if (!has_textfont)
+        return false;    
 
+    if (!x || !y || x == w - 1 || y == h - 1)
+       return true;
+
+#define IS_SCREEN(_sc) df::_sc::_identity.is_direct_instance(ws) 
+
+    if (IS_SCREEN(viewscreen_dwarfmodest))
+    {
         uint8_t menu_width, area_map_width;
         Gui::getMenuWidth(menu_width, area_map_width);
         int32_t menu_left = w - 1, menu_right = w - 1;
@@ -226,23 +234,20 @@ bool is_text_tile(int x, int y, bool &is_map)
             {
                 // Make burrow symbols use graphics font
                 if ((y != 12 && y != 13 && !(x == menu_left + 2 && y == 2)) || x == menu_left || x == menu_right) 
-                    return has_textfont;
+                    return true;
             }
             else
-                return has_textfont;
+                return true;
         }
 
         is_map = (x > 0 && x < menu_left);
 
         return false;
     }
-
-    if (!has_textfont)
-        return false;
     
-    if (strict_virtual_cast<df::viewscreen_setupadventurest>(ws))
+    if (IS_SCREEN(viewscreen_setupadventurest))
     {
-        df::viewscreen_setupadventurest *s = strict_virtual_cast<df::viewscreen_setupadventurest>(ws);
+        df::viewscreen_setupadventurest *s = static_cast<df::viewscreen_setupadventurest*>(ws);
         if (s->subscreen != df::viewscreen_setupadventurest::Nemesis)
             return true;
         else if (x < 58 || x >= 78 || y == 0 || y >= 21)
@@ -251,7 +256,7 @@ bool is_text_tile(int x, int y, bool &is_map)
         return false;
     }
     
-    if (strict_virtual_cast<df::viewscreen_dungeonmodest>(ws))
+    if (IS_SCREEN(viewscreen_dungeonmodest))
     {
         //df::viewscreen_dungeonmodest *s = strict_virtual_cast<df::viewscreen_dungeonmodest>(ws);
         //TODO
@@ -262,7 +267,7 @@ bool is_text_tile(int x, int y, bool &is_map)
         return false;
     }
 
-    if (strict_virtual_cast<df::viewscreen_choose_start_sitest>(ws))
+    if (IS_SCREEN(viewscreen_choose_start_sitest))
     {
         if (y <= 1 || y >= h - 6 || x == 0 || x >= 57)
             return true;
@@ -270,7 +275,7 @@ bool is_text_tile(int x, int y, bool &is_map)
         return false;
     }
         
-    if (strict_virtual_cast<df::viewscreen_new_regionst>(ws))
+    if (IS_SCREEN(viewscreen_new_regionst))
     {
         if (y <= 1 || y >= h - 2 || x <= 37 || x == w - 1)
             return true;
@@ -278,7 +283,7 @@ bool is_text_tile(int x, int y, bool &is_map)
         return false;
     }
 
-    if (strict_virtual_cast<df::viewscreen_layer_export_play_mapst>(ws))
+    if (IS_SCREEN(viewscreen_layer_export_play_mapst))
     {
         if (x == w - 1 || x < w - 23)
             return true;
@@ -286,7 +291,7 @@ bool is_text_tile(int x, int y, bool &is_map)
         return false;
     }
 
-    if (strict_virtual_cast<df::viewscreen_overallstatusst>(ws))
+    if (IS_SCREEN(viewscreen_overallstatusst))
     {
         if ((x == 46 || x == 71) && y >= 8)
             return false;
@@ -294,13 +299,21 @@ bool is_text_tile(int x, int y, bool &is_map)
         return true;
     }
 
-    /*if (strict_virtual_cast<df::viewscreen_petst>(ws))
+    if (IS_SCREEN(viewscreen_movieplayerst))
+    {
+        df::viewscreen_movieplayerst *s = static_cast<df::viewscreen_movieplayerst*>(ws);
+        return !s->is_playing;
+    }    
+
+    /*if (IS_SCREEN(viewscreen_petst))
     {
         if (x == 41 && y >= 7)
             return false;
 
         return true;
     }*/
+
+    //*out2 << Core::getInstance().p->readClassName(*(void**)ws) << std::endl;
 
     return true;
 }
@@ -572,8 +585,19 @@ void unhook()
     df::renderer* renderer = enabler->renderer;
     long **rVtable = (long **)enabler->renderer;
 
+#ifdef WIN32
+    HANDLE process = ::GetCurrentProcess();
+    DWORD protection = PAGE_READWRITE;
+    DWORD oldProtection;
+    if ( ::VirtualProtectEx( process, rVtable[0], 4*sizeof(void*), protection, &oldProtection ) )
+    {
+#endif
     rVtable[0][0] = (long)update_tile_old;
     rVtable[0][2] = (long)render_old;
+#ifdef WIN32
+    VirtualProtectEx( process, rVtable[0], 4*sizeof(void*), oldProtection, &oldProtection );
+    }
+#endif
 
     enabled = false;
     gps->force_full_display_count = true;
@@ -743,6 +767,108 @@ bool load_overrides()
     return found;
 }
 
+#ifndef WIN32
+//0x0079cb2a+4 0x14 - item name length
+//0x0079cb18+3 0x14 - item name length
+
+//0x0079e04e+3 0x1a - price, affects both sides
+//0x0079cbd1+2 0x1f - weight, affects both sides
+//0x0079ccbc+2 0x21 - [T], affects both sides
+
+//0x0079ef96+2 0x29 - "Value:"
+//0x0079d84d+2 0x39 - "Max weight"
+//0x0079d779+2 0x2a - "offer marked"
+//0x0079d07c+2 0x2a - "view good"
+
+//0x0079c314+1 0x3b - our side name (center)
+//0x0079cd6b+7 0x28 - our item name (+2)
+
+//0x002e04cf+2 0x27 - border left
+//0x002e0540+2 XXXX - border right
+
+struct traderesize_hook : public df::viewscreen_tradegoodsst
+{
+    typedef df::viewscreen_tradegoodsst interpose_base;
+
+    DEFINE_VMETHOD_INTERPOSE(void, render, ())
+    {
+        static bool checked = false, ok = false;
+
+        if (!checked)
+        {
+            checked = true;
+
+            //check only some of the addresses
+            ok =
+                *(unsigned char*)(0x002e04cf+2) == 0x27 &&
+                *(unsigned char*)(0x0079ef96+2) == 0x29 &&
+                *(unsigned char*)(0x0079cbd1+2) == 0x1f &&
+                *(unsigned char*)(0x0079cb2a+4) == 0x14;
+
+            if (ok)
+            {
+                //fixing drawing of the right border
+                unsigned char t1[] = { 0x6b, 0xd1, 0x00 }; //imul edx, ecx, XXX
+                Core::getInstance().p->patchMemory((void*)(0x002e0540), t1, sizeof(t1));
+
+                unsigned char t2[] = { 0x01, 0xf2, 0x90 }; //add edx, esi; nop
+                Core::getInstance().p->patchMemory((void*)(0x002e0545), t2, sizeof(t2));
+            }
+        }
+
+        if (ok)
+        {
+            static int lastw = -1;
+            if (gps->dimx != lastw)
+            {
+                lastw = gps->dimx;
+
+                unsigned char x1 = lastw/2-1, x;
+
+                //border
+                x = x1;
+                Core::getInstance().p->patchMemory((void*)(0x002e04cf+2), &x, 1);
+                x = x1 + 1;
+                Core::getInstance().p->patchMemory((void*)(0x002e0540+2), &x, 1);
+
+                x = x1 + 1 + 2;
+                Core::getInstance().p->patchMemory((void*)(0x0079d07c+2), &x, 1); //view good
+                Core::getInstance().p->patchMemory((void*)(0x0079d779+2), &x, 1); //offer marked
+
+                x = x1 + 1 + 1;
+                Core::getInstance().p->patchMemory((void*)(0x0079ef96+2), &x, 1); //value
+
+                x = x1 + 1 + 1 + 16;
+                Core::getInstance().p->patchMemory((void*)(0x0079d84d+2), &x, 1); //max weight            
+
+                x = x1 + 1 + 2 - 2;
+                Core::getInstance().p->patchMemory((void*)(0x0079cd6b+7), &x, 1); //item name
+
+                x = x1 - 2 - 3;
+                Core::getInstance().p->patchMemory((void*)(0x0079ccbc+2), &x, 1); //[T]
+
+                x = x1 - 2 - 3;
+                Core::getInstance().p->patchMemory((void*)(0x0079cbd1+2), &x, 1); //item weight
+
+                x = x1 - 2 - 3 - 5;
+                Core::getInstance().p->patchMemory((void*)(0x0079e04e + 3), &x, 1); //item price
+
+                x = x1 - 2 - 3 - 5 - 5;
+                Core::getInstance().p->patchMemory((void*)(0x0079cb2a+4), &x, 1); //item name len
+                Core::getInstance().p->patchMemory((void*)(0x0079cb18+3), &x, 1); //item name len
+
+                x = (x1 + 2 + lastw) / 2;
+                Core::getInstance().p->patchMemory((void*)(0x0079c314+1), &x, 1); //our side name
+            }
+        }
+
+        INTERPOSE_NEXT(render)();
+    }
+};
+
+IMPLEMENT_VMETHOD_INTERPOSE(traderesize_hook, render);
+#endif
+
 DFhackCExport command_result plugin_init ( color_ostream &out, vector <PluginCommand> &commands)
 {
     out2 = &out;
@@ -778,6 +904,10 @@ DFhackCExport command_result plugin_init ( color_ostream &out, vector <PluginCom
     if (has_textfont || has_overrides)
         hook();
 
+#ifndef WIN32
+    INTERPOSE_HOOK(traderesize_hook, render).apply(true);
+#endif
+
     return CR_OK;
 }
 
@@ -785,6 +915,10 @@ DFhackCExport command_result plugin_shutdown ( color_ostream &out )
 {
     if (enabled)
         unhook();
+
+#ifndef WIN32
+    INTERPOSE_HOOK(traderesize_hook, render).apply(false);
+#endif    
 
     return CR_OK;
 }
