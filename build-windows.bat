abd15cdd45e8ca55cd80103e338de26426f3974b
TheBloke <tomjobbins@gmail.com>
2015-12-28 06:17:55 +0000

    Incredibly basic Windows build script

---
+++ build-windows.bat
@@ -0,0 +1,8 @@
+call "%VS100COMNTOOLS%vsvars32.bat"
+
+set DFHACKDIR="C:\Users\tomjo\Documents\code\dfhack-4203-alpha1"
+set DFHACKVER=0.42.03-alpha1
+set DFVERNUM=04203
+set TWBT_VER=5.52-alpha
+
+msbuild /p:Platform=Win32 /p:Configuration=Release /p:dfhack=%DFHACKDIR% /p:dfhackver=%DFHACKVER% /p:twbt_ver=%TWBT_VER% /p:dfvernum=%DFVERNUM% twbt.vcxproj
